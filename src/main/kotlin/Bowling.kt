547d4524a03c9f7605e32a0096330cb993ec8e57
andys <andy.smith@mkodo.com>
2019-04-11 21:17:28 +0100

    Clean up

--- src/main/kotlin/Bowling.kt
+++ src/main/kotlin/Bowling.kt
@@ -2,27 +2,8 @@ class Bowling {
 
     fun score(s: String): Int {
         val rolls = convertInputToRolls(s)
-
-        var score = 0
-        var i = 0
-        for (frames in 0 until 10) {
-            if (isSpare(rolls, i)) {
-                score += 10 + rolls[i + 2]
-                i++
-            } else if (isStrike(rolls, i)) {
-                score += 10 + rolls[i + 1] + rolls[i + 2]
-            } else {
-                score += rolls[i] + rolls[i + 2]
-            }
-            i++
-        }
-
-        return score
+        return calulcate(rolls)
     }
-
-    private fun isSpare(rolls: IntArray, i: Int) = rolls[i] + rolls[i + 1] == 10
-
-    private fun isStrike(rolls: IntArray, i: Int) = rolls[i] == 10
 
     fun convertInputToRolls(input: String): IntArray {
         val strippedInput = input.replace(" ", "")
@@ -30,15 +11,7 @@ class Bowling {
         var i = 0
         var previousChar = ' '
         for (char in strippedInput) {
-            if (char == 'X' ) {
-                arrayList.add(10)
-            } else if (char == '/') {
-                arrayList.add(10 - convertCharToRoll(previousChar))
-            } else if (char == '-') {
-                arrayList.add(0)
-            } else {
-                arrayList.add(convertCharToRoll(char))
-            }
+            convertCharToRollValue(char, arrayList, previousChar)
             previousChar = char
             i++
         }
@@ -46,8 +19,45 @@ class Bowling {
         return arrayList.toIntArray()
     }
 
+    private fun convertCharToRollValue(char: Char, arrayList: ArrayList<Int>, previousChar: Char) {
+        if (char == 'X') {
+            arrayList.add(10)
+        } else if (char == '/') {
+            arrayList.add(10 - convertCharToRoll(previousChar))
+        } else if (char == '-') {
+            arrayList.add(0)
+        } else {
+            arrayList.add(convertCharToRoll(char))
+        }
+    }
+
     private fun convertCharToRoll(previousChar: Char) = Integer.parseInt(previousChar.toString())
 
+    private fun calulcate(rolls: IntArray): Int {
+        var score = 0
+        var i = 0
+        for (frames in 0 until 10) {
+            if (isSpare(rolls, i)) {
+                score += calculateSpare(rolls, i)
+                i++
+            } else if (isStrike(rolls, i)) {
+                score += calculateStrike(rolls, i)
+            } else {
+                score += calculateFrame(rolls, i)
+            }
+            i++
+        }
+        return score
+    }
 
+    private fun isSpare(rolls: IntArray, i: Int) = rolls[i] + rolls[i + 1] == 10
+
+    private fun calculateSpare(rolls: IntArray, i: Int) = 10 + rolls[i + 2]
+
+    private fun isStrike(rolls: IntArray, i: Int) = rolls[i] == 10
+
+    private fun calculateStrike(rolls: IntArray, i: Int) = 10 + rolls[i + 1] + rolls[i + 2]
+
+    private fun calculateFrame(rolls: IntArray, i: Int) = rolls[i] + rolls[i + 2]
 
 }
